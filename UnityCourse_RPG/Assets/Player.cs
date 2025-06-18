b61cd717509b19f748fa0ef5aafa09d7bf8c7c9d
_tireDep <wing997@naver.com>
2025-06-19 00:51:38 +0900

    New States & Structure

--- UnityCourse_RPG/Assets/Player.cs
+++ UnityCourse_RPG/Assets/Player.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private StateMachine stateMachine;
-    private EntityState idleState;
+    public Player_IdleState idleState {  get; private set; }
+    public Player_MoveState moveState { get; private set; }
 
     private void Awake()
     {
         stateMachine = new StateMachine();
 
-        idleState = new EntityState(stateMachine, "Idle State");
+        idleState = new Player_IdleState(this, stateMachine, "Idle");
+        moveState = new Player_MoveState(this, stateMachine, "Move");
     }
 
     private void Start()
---
+++ UnityCourse_RPG/Assets/Player_MoveState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player_MoveState : EntityState
+{
+    public Player_MoveState(Player player, StateMachine stateMachine, string stateName) : base(player, stateMachine, stateName)
+    {
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
+    }
+}
