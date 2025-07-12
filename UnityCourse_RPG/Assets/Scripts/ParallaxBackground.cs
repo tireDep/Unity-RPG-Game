884008fb2dfe507f2280366156cdb456a9859b9d
_tireDep <wing997@naver.com>
2025-07-12 22:40:25 +0900

    Endless Background - 배경 흔들림, 플레이어 흔들림 수정(Fixed Update로 수정)

--- UnityCourse_RPG/Assets/Scripts/ParallaxBackground.cs
+++ UnityCourse_RPG/Assets/Scripts/ParallaxBackground.cs
@@ -7,20 +7,37 @@ public class ParallaxBackground : MonoBehaviour
 
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
+    private float cameraHalfWidth;
+
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect; // 직교 크기 * 메인 카메라 측면
+        CalculateImageLength();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        // 배경 흔들림 수정을 위해 FixedUpdate로 변경
         float currentCameraPositionX = mainCamera.transform.position.x;
         float distanceToMove = currentCameraPositionX - lastCameraPositionX;
         lastCameraPositionX = currentCameraPositionX;
 
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
         foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void CalculateImageLength()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }
--- UnityCourse_RPG/Assets/Scripts/ParallaxLayer.cs
+++ UnityCourse_RPG/Assets/Scripts/ParallaxLayer.cs
@@ -6,8 +6,33 @@ public class ParallaxLayer
     [SerializeField] private Transform background;
     [SerializeField] private float parallaxMultiplier;
 
+    private float imageFullWidth;
+    private float imageHalfWidth;
+    [SerializeField] private float imageWidthOffset = 10.0f;
+
     public void Move(float distanceToMove)
     {
         background.position += Vector3.right * (distanceToMove * parallaxMultiplier);
+    }
+
+    public void CalculateImageWidth()
+    {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth * 0.5f;
+    }
+
+    public void LoopBackground(float cameraLeftEdge, float cameraRightEdge)
+    {
+        float imageRightEdge = (background.position.x + imageHalfWidth) - imageWidthOffset;
+        float imageLeftEdge = (background.position.x - imageHalfWidth) + imageWidthOffset;
+
+        if (imageRightEdge < cameraLeftEdge)
+        {
+            background.position += Vector3.right * imageFullWidth;
+        }
+        else if (imageLeftEdge > cameraRightEdge) 
+        {
+            background.position += Vector3.right * -imageFullWidth;
+        }
     }
 }
